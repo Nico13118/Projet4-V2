8bd6509d003083bb28294cecf20f7a4476c1b088
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-09 15:44:24 +0200

    Importation de la classe MainController du répertoire controllers et du fichier maincontroller.py. Ajout de la méthode main avec l'ajout de d'une nouvelle instance de classe et appel de la méthode run du fichier maincontroller.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from controllers.maincontroller import MainController
+
+
+def main():
+    main_controller = MainController()
+    main_controller.run()
+
+
+if __name__ == "__main__":
+    main()
