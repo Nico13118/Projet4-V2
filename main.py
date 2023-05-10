cbd1d2bf5b06e5d6f3ec1ca77e2107e885c05b49
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-10 16:32:55 +0200

    Importation de la classe DirectoryModel. Ajout d'une nouvelle instance de classe Directorymodel et appelde la méthode make_directory1 qui permet de créer plusieurs répertoire si manquant

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from controllers.maincontroller import MainController
+from models import DirectoryModel
 
 
 def main():
     main_controller = MainController()
+    directory_model = DirectoryModel()
+    directory_model.make_directory1()
     main_controller.run()
 
 
