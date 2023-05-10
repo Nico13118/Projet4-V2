209442b35dcbf5008b0b24dc84a78fa67dbd9b65
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-10 16:46:38 +0200

    Création de la classe TournamentView et création de plusieurs méthode qui permet de récupérer les informations d'un tournoi lors de sa création et quelques messages d'erreur

---
+++ controllers/databasecontroller.py
@@ -0,0 +1,13 @@
+# import os
+#
+# database_players = "List_Registered_Players.json"
+# database_tournament = "Tournament_Info.json"
+# database_match = "List_Match.json"
+# database_score = "List_Score.json"
+# database_rounds = "Round.json"
+#
+# data = os.getcwd()
+# path_database_players = f"{data}/tournament/{tournament_name/}"
+#
+# class DatabaseController:
+
