fcb0f950ce7165c571c08a89f4965cefab226942
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-10 16:17:19 +0200

    Création d'une nouvelle instance de la classe TournamentController avec comme paramètre TournamentView(). Configuration en cours de la nouvelle instance de la classe PlayerController

--- controllers/maincontroller.py
+++ controllers/maincontroller.py
@@ -1,10 +1,12 @@
-from controllers import MenuController
-from views import MenuView
+from controllers import MenuController, PlayerController, TournamentController
+from views import MenuView, TournamentView
 
 
 class MainController:
     def __init__(self):
         self.menu_controller = MenuController(MenuView(), self)
+        self.tournament_controller = TournamentController(TournamentView(), self)
+        self.player_controller = PlayerController()
 
     def run(self):
         self.menu_controller.run()
