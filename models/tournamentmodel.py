f7ec4ccf676548e7d994b6795b9eee9ab152b22b
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-10 16:38:50 +0200

    Création de la classe TournamentModel et de la méthode tournament_registration -Configuration en cours-

---
+++ models/tournamentmodel.py
@@ -0,0 +1,14 @@
+class TournamentModel:
+    def __init__(self, tournament_name, place, start_date, end_date, director_remark):
+        self.tournament_name = tournament_name
+        self.place = place
+        self.start_date = start_date
+        self.end_date = end_date
+        self.director_remark = director_remark
+        self.tournament = []
+
+    def tournament_registration(self):
+        pass
+
+
+
