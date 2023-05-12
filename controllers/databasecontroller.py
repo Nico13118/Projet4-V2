c770e4902ecc611bc75545aa9e5e783d2b9b0900
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-12 10:40:39 +0200

    Importation de la classe TournamentModel et PlayerModel. Importation de la bibliothèque os et json. Création de la classe DatabaseController. Ajout de la méthode add_tournament_in_json, méthode qui permet d'ajouter les informations du tournoi dans un fichier json. Ajout de la méthode aa_player_in_json, méthode qui permet d'enregistrer les informations des joueurs inscrits, la méthode peut continuer à ajouter des joueurs dans le fichier json

--- controllers/databasecontroller.py
+++ controllers/databasecontroller.py
@@ -1,13 +1,43 @@
-# import os
-#
-# database_players = "List_Registered_Players.json"
-# database_tournament = "Tournament_Info.json"
-# database_match = "List_Match.json"
-# database_score = "List_Score.json"
-# database_rounds = "Round.json"
-#
-# data = os.getcwd()
-# path_database_players = f"{data}/tournament/{tournament_name/}"
-#
-# class DatabaseController:
+from models import TournamentModel, PlayerModel
+
+import os
+import json
+# Liste des joueurs inscrits : "List_Registered_Players.json" --data\tournament
+# Information d'un tournoi lors de sa création : "Tournament_Info.json" --data\tournament\nom_du_tournoi\
+# Liste des rencontres : "List_Match.json"
+# Liste des scores après chaque rencontre : "List_Score.json"
+# Fin du round "Round.json"
+
+
+class DatabaseController:
+    def add_tournament_in_json(self, tournamentmodel):
+        data = os.getcwd()
+        path = f"{data}/data/tournament/"
+        directory = os.listdir(path)
+        tournament_name = str(directory[0])
+        tournament_model = TournamentModel
+        tournament = tournament_model.tournament_registration(tournamentmodel)
+        with open(f"{data}/data/tournament/{tournament_name}/Tournament_Info.json", "w") as f:
+            json.dump(tournament, f)
+
+    def add_player_in_json(self, playermodel):
+        data = os.getcwd()
+        path = f"{data}/data/tournament/players_list/"
+        directory1 = os.listdir(path)
+        # Si le fichier json n'existe pas alors enregistre les données dans le fichier json
+        if not directory1:
+            player_model = PlayerModel
+            player = player_model.player_registration(playermodel)
+            with open(f"{path}/List_Registered_Players.json", "w") as f:
+                json.dump(player, f)
+        # Sinon si le fichier existe, alors extraire les données puis enregistrer l'ensemble dans le même fichier
+        else:
+            player_model = PlayerModel
+            player = player_model.player_registration(playermodel)
+            with open(f"{path}/List_Registered_Players.json", "r") as f:
+                list_player = json.load(f)
+                for new_player_list in zip(player, list_player):
+                    with open(f"{path}/List_Registered_Players.json", "w") as g:
+                        json.dump(new_player_list, g)
+
 
