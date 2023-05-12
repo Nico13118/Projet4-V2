904ea420641869d5912668db7343f0e044009d0c
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-12 10:43:08 +0200

    Ajout de nouvelles importation de classe. Reconfiguration du controleur

--- controllers/maincontroller.py
+++ controllers/maincontroller.py
@@ -1,12 +1,15 @@
-from controllers import MenuController, PlayerController, TournamentController
-from views import MenuView, TournamentView
+from controllers import MenuController, PlayerController, TournamentController, DatabaseController
+from views import MenuView, TournamentView, PlayerView
+from models import DirectoryModel
 
 
 class MainController:
     def __init__(self):
-        self.menu_controller = MenuController(MenuView(), self)
-        self.tournament_controller = TournamentController(TournamentView(), self)
-        self.player_controller = PlayerController()
+        self.dm = DirectoryModel()
+        self.db = DatabaseController()
+        self.menu_controller = MenuController(MenuView(), self, self.db, self.dm)
+        self.tournament_controller = TournamentController(TournamentView(), self, self.db, self.dm)
+        self.player_controller = PlayerController(PlayerView(), self, self.db, self.dm)
 
     def run(self):
         self.menu_controller.run()
