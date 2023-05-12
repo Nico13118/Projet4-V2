f5a4b622814120c2ad6fc6f473895ad660db8739
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-12 11:03:02 +0200

    Création de la classe PlayerModel. Ajout de la méthode player_registration qui permet d'enregistrer les données sous le bon format puis dans une liste.

---
+++ models/playermodel.py
@@ -0,0 +1,25 @@
+class PlayerModel:
+    def __init__(self, chess_id, name, first_name, birthday, number_player=0, rounds=0, score=0.0):
+        self.chess_id = chess_id
+        self.name = name
+        self.first_name = first_name
+        self.birthday = birthday
+        self.number_player = number_player
+        self.rounds = rounds
+        self.score = score
+        self.player = []
+
+    def player_registration(self):
+        player_information = {
+            "identifiant": self.chess_id,
+            "nom": self.name,
+            "prenom": self.first_name,
+            "naissance": self.birthday,
+            "joueur": self.number_player,
+            "round": self.rounds,
+            "score": self.score
+        }
+        self.player.append(player_information)
+        player = self.player
+        return player
+
