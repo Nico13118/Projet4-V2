2f0efa1a09cda1aff4693dd59310a460cc742749
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-12 10:47:54 +0200

    Correction apportée au niveau de la condition if user_input == 1. Ajout d'un appel d'une méthode qui permet d'enregistrer les joueurs au niveau du choix 2.

--- controllers/menucontroller.py
+++ controllers/menucontroller.py
@@ -1,23 +1,25 @@
 
 class MenuController:
-    def __init__(self, view, controller):
+    def __init__(self, view, controller, database, directory):
         self.view = view
         self.controller = controller
+        self.db = database
+        self.dm = directory
 
     def run(self):
         self.view.club_name()
         user_input = self.view.get_user_input()
-        if user_input == 1:
+        if user_input == "1":
             """Vérifier si l'utilisateur peut créer un tournoi"""
             self.controller.tournament_controller.control()
 
-        elif user_input == 2:
-            print("Test2")
-        elif user_input == 3:
+        elif user_input == "2":
+            self.controller.player_controller.player_message()
+        elif user_input == "3":
             print("Test3")
-        elif user_input == 4:
+        elif user_input == "4":
             print("Test4")
-        elif user_input == 5:
+        elif user_input == "5":
             quit()
         else:
             self.view.tournament_error_message2()
