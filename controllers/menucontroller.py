92aa9d3f8790ed52ee0749b809021039ba518b1d
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-15 16:22:40 +0200

    Modification du nom de la méthode def run en def run_menu. Ajout d'une nouvelle méthode def run_menu_player concernant un menu supplémentaire pour gérer d'autres options lié aux joueurs. Ajout d'une nouvelle méthode def run_menu_tournament concernant un menu supplémentaire pour gérer d'autres options lié au tournoi. Ajout d'une nouvelle méthode def run_menu_report concernant un menu supplémentaire pour gérer d'autres options lié aux rapports

--- controllers/menucontroller.py
+++ controllers/menucontroller.py
@@ -6,7 +6,7 @@ class MenuController:
         self.db = database
         self.dm = directory
 
-    def run(self):
+    def run_menu(self):
         self.view.club_name()
         user_input = self.view.get_user_input()
         if user_input == "1":
@@ -16,16 +16,52 @@ class MenuController:
         elif user_input == "2":
             self.controller.player_controller.player_message()
         elif user_input == "3":
-            print("Test3")
+            self.run_menu_player()
         elif user_input == "4":
-            print("Test4")
+            self.run_menu_tournament()
         elif user_input == "5":
+            print("Test4")
+        elif user_input == "6":
             quit()
         else:
             self.view.tournament_error_message2()
-            self.run()
+            self.run_menu()
 
+    def run_menu_player(self):
+        self.view.club_name()
+        user_input = self.view.get_user_input2()
+        if user_input == "1":
+            self.controller.player_list_controller.print_player_list_controller()
+            self.run_menu_player()
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            self.controller.menu_controller.run_menu()
+        else:
+            self.run_menu()
 
+    def run_menu_tournament(self):
+        self.view.club_name()
+        user_input = self.view.get_user_input3()
+        if user_input == "1":
+            pass
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            self.controller.tournament_controller.del_tournament_controller1()
+        elif user_input == "4":
+            self.run_menu()
+        else:
+            self.run_menu()
 
-
-
+    def run_menu_report(self):
+        self.view.club_name()
+        user_input = self.view.get_user_input4()
+        if user_input == "1":
+            pass
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            self.run_menu()
+        else:
+            self.run_menu()
