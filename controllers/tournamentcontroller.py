35ea93766d3fbb4e2fde5ad9cb535749aa665eec
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-15 16:49:29 +0200

    Ajout de la méthode del_tournament_controller1 qui permet de détecter la présence du répertoire lié à un tournoi. Si le fichier existe alors il fait appel à une autre méthode. Ajout de la méthode del_tournament_controller2 qui vérifie la saisie de l'utilisateur et fait appel à une autre méthode afin de procéder à la suppression d'un tournoi

--- controllers/tournamentcontroller.py
+++ controllers/tournamentcontroller.py
@@ -22,7 +22,7 @@ class TournamentController:
         directory1 = os.listdir(path)
         if directory1:
             self.view.tournament_error_message1()
-            self.controller.menu_controller.run()
+            self.controller.menu_controller.run_menu()
 
         else:
             """Création d'un tournoi"""
@@ -83,8 +83,34 @@ class TournamentController:
 
         )
         self.db.add_tournament_in_json(tournamentmodel)
-        self.controller.menu_controller()
+        self.controller.menu_controller.run_menu()
 
+    def del_tournament_controller1(self):
+        data = os.getcwd()
+        path = f"{data}/data/tournament/"
+        directory1 = os.listdir(path)
+        if directory1:
+            self.view.del_tournament_message_view1()
+            self.del_tournament_controller2()
+
+        else:
+            self.view.del_tournament_message_view4()
+            self.controller.menu_controller.run_menu_tournament()
+
+    def del_tournament_controller2(self):
+        user_input = self.view.del_tournament_view()
+        if user_input == "":
+            self.view.tournament_error_message2()
+        elif user_input == "Y" or user_input == "y" or user_input == "O" or user_input == "o" or user_input == "oui":
+            self.db.del_tournament_db()
+            self.view.del_tournament_message_view3()
+            self.controller.menu_controller.run_menu_tournament()
+
+        elif user_input == "N" or user_input == "n" or user_input == "No" or user_input == "no" or user_input == "non":
+            self.controller.menu_controller.run_menu_tournament()
+        else:
+            self.view.del_tournament_message_view2()
+            self.del_tournament_controller2()
 
 
 
