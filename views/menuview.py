765f18bd499ebd498675ede88075e7b026a62406
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-15 16:53:39 +0200

    Ajout de 3 méthodes avec 3 nouveaux menu

--- views/menuview.py
+++ views/menuview.py
@@ -16,19 +16,67 @@ class MenuView:
 
                     """
             print("\n|================== MENU ==================|")
-            print("|1 -Créer un tournoi.                      |")
-            print("|2 -Inscription des joueurs                |")
-            print("|3 -Démarrer un tournoi.                   |")
-            print("|4 -Reprendre un tournoi.                  |")
-            print("|5 -Visualiser le rapport d'un tournoi.    |")
-            print("|6 -Supprimer un tournoi.                  |")
-            print("|7 -Supprimer un joueur.                   |")
-            print("|8  -Quitter l'application.                 |")
-            print("|==========================================|")
+            print("| 1 -Créer un tournoi.                      |")
+            print("| 2 -Inscription des joueurs                |")
+            print("| 3 -Gérer les joueurs                      |")
+            print("| 4 -Gérer les tournois                     |")
+            print("| 5 -Rapport de tournoi                     |")
+            print("| 6 -Quitter l'application.                 |")
+            print("|===========================================|")
 
         print_menu()
         user_input = input("Faite votre choix :")
-        if user_input > "5":
+        if int(user_input) > 6:
+            print("Choix invalide !!!")
+        else:
+            return user_input
+
+
+    def get_user_input2(self):
+
+        def print_menu_player():
+            print("\n|============ Gérer les joueurs ===========|")
+            print("| 1 -Voir la liste des joueurs              |")
+            print("| 2 -Supprimer un joueur de la liste        |")
+            print("| 3 -Retourner au menu principal            |")
+            print("|===========================================|")
+
+        print_menu_player()
+        user_input = input("Faite votre choix :")
+        if int(user_input) > 3:
+            print("Choix invalide !!!")
+        else:
+            return user_input
+
+    def get_user_input3(self):
+
+        def print_menu_tournament():
+            print("\n|=========== Gérer les tournois ===========|")
+            print("| 1 -Démarrer un tournoi                    |")
+            print("| 2 -Reprendre un tournoi                   |")
+            print("| 3 -Supprimer un tournoi                   |")
+            print("| 4 -Retourner au menu principal            |")
+            print("|===========================================|")
+
+        print_menu_tournament()
+        user_input = input("Faite votre choix :")
+        if int(user_input) > 4:
+            print("Choix invalide !!!")
+        else:
+            return user_input
+
+    def get_user_input4(self):
+
+        def print_menu_report():
+            print("\n|=========== Rapport de tournoi ===========|")
+            print("| 1 -Rapport du tournoi en cours            |")
+            print("| 2 -Rapport des tournois précédents.       |")
+            print("| 3 -Retourner au menu principal            |")
+            print("|===========================================|")
+
+        print_menu_report()
+        user_input = input("Faite votre choix :")
+        if int(user_input) > 3:
             print("Choix invalide !!!")
         else:
             return user_input
