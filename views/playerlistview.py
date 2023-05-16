d183829f8c0fd59c13ac8d76b0bb96cae36b8170
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-16 16:14:16 +0200

    Ajout de plusieurs méthodes qui affichent des message concernant la suppression d'un joueurs

--- views/playerlistview.py
+++ views/playerlistview.py
@@ -11,8 +11,22 @@ class PlayerListView:
         user_input = input("Saisir le numéro du joueur à intégrer au tournoi (1, 2, 3 ...) :")
         return user_input
 
+    def message_del_player(self):
+        user_input = input("Supprimer un joueur de la liste, souhaitez-vous continuer ? (Y(es) / N(o) :")
+        return user_input
+    def message_del_player_in_list_view(self):
+        user_input = input("Saisir le numéro du joueur à supprimer de la liste (1, 2, 3 ...) :")
+        user_input = int(user_input)
+        return user_input
+
+    def message_del_player_in_list_view2(self):
+        print("Joueur supprimé avec succes.")
+
     def message_error_list_view(self):
         print("Aucune liste à afficher")
+
+    def message_error_list_view2(self):
+        print("Choix invalide !!!")
 
     def print_player_list_view(self, list_player):
         i = 0
