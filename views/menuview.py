b4120e4ee3b69c32b080c0cfc086d17d06c95c9a
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-17 22:39:00 +0200

    Petit modification au niveau des menus.

--- views/menuview.py
+++ views/menuview.py
@@ -3,62 +3,46 @@ class MenuView:
     def club_name(self):
         print("\n******** Club Provence Échecs ********* ")
 
-    def error_message_menuview1(self):
-        print("Choix invalide !!!")
-
-    def error_message_menuview2(self):
-        print("La saisie ne peut pas être vide !!")
-
-    def get_user_input(self):
+    def get_user_input_main_menu(self): # A modifier
         """
+        Affichage du menu
         Récupération de la saisie utilisateur
         :return: Retourne la saisie utilisateur
         """
+        print("\n|============ MENU PRINCIPAL =============|")
+        print("| 1 -Créer un tournoi.                      |")
+        print("| 2 -Inscription des joueurs                |")
+        print("| 3 -Gérer les joueurs                      |")
+        print("| 4 -Gérer les tournois                     |")
+        print("| 5 -Rapport de tournoi                     |")
+        print("| 6 -Quitter l'application.                 |")
+        print("|===========================================|")
 
-        def print_menu():
-            """
-                    Affichage du menu
-
-                    """
-            print("\n|================== MENU ==================|")
-            print("| 1 -Créer un tournoi.                      |")
-            print("| 2 -Inscription des joueurs                |")
-            print("| 3 -Gérer les joueurs                      |")
-            print("| 4 -Gérer les tournois                     |")
-            print("| 5 -Rapport de tournoi                     |")
-            print("| 6 -Quitter l'application.                 |")
-            print("|===========================================|")
-
-        print_menu()
         user_input = input("Faite votre choix :")
         return user_input
 
-    def get_user_input2(self):
+    def get_user_input_player_menu(self):
 
-        def print_menu_player():
-            print("\n|============ Gérer les joueurs ===========|")
-            print("| 1 -Voir la liste des joueurs              |")
-            print("| 2 -Supprimer un joueur de la liste        |")
-            print("| 3 -Retourner au menu principal            |")
-            print("|===========================================|")
+        print("\n|============ Gérer les joueurs ===========|")
+        print("| 1 -Voir la liste des joueurs              |")
+        print("| 2 -Supprimer un joueur de la liste        |")
+        print("| 3 -Retourner au menu principal            |")
+        print("|===========================================|")
 
-        print_menu_player()
         user_input = input("Faite votre choix :")
         return user_input
 
-    def get_user_input3(self):
+    def get_user_input_tournament_menu(self):
 
-        def print_menu_tournament():
-            print("\n|=========== Gérer les tournois ===========|")
-            print("| 1 -Ajouter des joueurs au tournoi         |")
-            print("| 2 -Supprimer des joueurs du tournoi       |")
-            print("| 3 -Lancer le tournoi                      |")
-            print("| 4 -Reprendre un tournoi                   |")
-            print("| 5 -Supprimer un tournoi                   |")
-            print("| 6 -Retourner au menu principal            |")
-            print("|===========================================|")
+        print("\n|=========== Gérer les tournois ===========|")
+        print("| 1 -Ajouter des joueurs au tournoi         |")
+        print("| 2 -Supprimer des joueurs du tournoi       |")
+        print("| 3 -Lancer le tournoi                      |")
+        print("| 4 -Reprendre un tournoi                   |")
+        print("| 5 -Supprimer un tournoi                   |")
+        print("| 6 -Retourner au menu principal            |")
+        print("|===========================================|")
 
-        print_menu_tournament()
         user_input = input("Faite votre choix :")
         return user_input
 
@@ -74,3 +58,9 @@ class MenuView:
         print_menu_report()
         user_input = input("Faite votre choix :")
         return user_input
+
+    def error_message_menuview1(self):
+        print("Choix invalide !!!")
+
+    def error_message_menuview2(self):
+        print("La saisie ne peut pas être vide !!")
