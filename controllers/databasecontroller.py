45f1282ad3ead2b874bff6f6bf0caec74fde6ad3
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-17 22:14:53 +0200

    Configuration de la méthode add_player_in_tournament_db afin d'extraire et triés une liste, selectionner 1 jour dans une liste, supprimer le joueur dans une autre liste, puis d'enregistrer les 2 listes dans 2 fichiers json

--- controllers/databasecontroller.py
+++ controllers/databasecontroller.py
@@ -22,6 +22,9 @@ class DatabaseController:
             json.dump(tournament, f)
 
     def add_player_in_json(self, playermodel):
+        """Méthode qui enregistre les joueurs dans la base de données, méthode qui peut ajouter des joueurs
+        au fur et à mesure
+        """
         temporarylist = []
         data = os.getcwd()
         path = f"{data}/data/players_list/"
@@ -99,4 +102,76 @@ class DatabaseController:
         del player_sorted[user_input]
         self.add_player_in_json2(player_sorted)
 
+    def add_player_in_tournament_db(self, user_input):
+        temporary_list = []
+        user_input = int(user_input)
+        user_input -= 1
+        list_player = self.get_player_list()  # Extraire les données de la liste
+        player_sorted = self.sort_player_list_db(list_player)  # Tries la liste des joueurs par ordre alphabétique
+        player_select = player_sorted[user_input]  # Selection du joueur
+        temporary_list.append(player_select)
+        self.add_player_select_in_json(temporary_list) # Enregistrement du joueur selectionné dans List_Players_Select.json
+        del player_sorted[user_input] # Supprime le joueur selectionner de la liste player_sorted
+        new_list_player = player_sorted
+        self.add_player_temporary_in_json(new_list_player) #Enregistrement des joueurs restant dans Temporary_List_Players.json
 
+    def add_player_in_tournament_db2(self, user_input):
+        temporary_player_select = []
+        user_input -= 1
+
+        list_player_select = self.get_list_player_select_db()  # Récupération des données de list_player_select
+        for list_player_select1 in list_player_select:
+            temporary_player_select.append(list_player_select1)
+        temporary_list = self.get_temporary_list_player_db()  # Récupération des données de temporary_list
+        player_select = temporary_list[user_input] # Selection du joueur à ajouter dans le tournoi
+
+        temporary_player_select.append(player_select)
+
+        del temporary_list[user_input] # Suppression du joueur de cette liste
+
+        self.add_player_select_in_json(temporary_player_select) # Enregistrement de la liste dans List_Players_Select.json
+        self.add_player_temporary_in_json(temporary_list) # Enregistrement de la liste dans Temporary_List_Players.json
+
+    def add_player_select_in_json(self, player_select):
+        """Enregistrement du joueur selectionné dans List_Players_Select.json"""
+
+        data = os.getcwd()
+        path = f"{data}/data/tournament/"
+        directory = os.listdir(path)
+        tournament_name = str(directory[0])
+
+        path2 = (f"{data}/data/tournament/{tournament_name}/Player_Select")
+        with open(f"{path2}/List_Players_Select.json", "w") as f:
+            json.dump(player_select, f)
+
+    def add_player_temporary_in_json(self, new_list_player):
+        """Enregistrement de la nouvelle liste avec le joueur en moins dans Temporary_List_Players.json"""
+        data = os.getcwd()
+        path = f"{data}/data/tournament/"
+        directory = os.listdir(path)
+        tournament_name = str(directory[0])
+        path2 = (f"{data}/data/tournament/{tournament_name}/Player_Select")
+        with open(f"{path2}/Temporary_List_Players.json", "w") as f:
+            json.dump(new_list_player, f)
+
+    def get_list_player_select_db(self):
+        """Récupération des données du fichier List_Players_Select.json """
+        data = os.getcwd()
+        path = f"{data}/data/tournament/"
+        directory = os.listdir(path)
+        tournament_name = str(directory[0])
+        path2 = (f"{data}/data/tournament/{tournament_name}/Player_Select")
+        with open(f"{path2}/List_Players_Select.json", "r") as f:
+            list_player_select = json.load(f)
+            return list_player_select
+
+    def get_temporary_list_player_db(self):
+        """Récupération des données du fichier Temporary_List_Players.json  """
+        data = os.getcwd()
+        path = f"{data}/data/tournament/"
+        directory = os.listdir(path)
+        tournament_name = str(directory[0])
+        path2 = (f"{data}/data/tournament/{tournament_name}/Player_Select")
+        with open(f"{path2}/Temporary_List_Players.json", "r") as f:
+            temporary_list = json.load(f)
+            return temporary_list
