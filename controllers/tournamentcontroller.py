e31b48435e1e849914964f143cab7893d251a003
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-18 23:37:16 +0200

    Ajout de la méthode start_tournament, la méthode controle si le fichier List_Players_Select.json existe, Controle qu'ily a bien 8 joueuyrs dans cette liste, puis appel la méthode shuflle_players qui procède au mélange des joueurs, assigne une couleur aux joueurs, appel une méthode pour enregistrter ces données dans un fichier json. Ajout de la méthode create_teams, cette méthode récupère les données de list_Match pour envoyer ces informations dans tournamentview

--- controllers/tournamentcontroller.py
+++ controllers/tournamentcontroller.py
@@ -1,6 +1,7 @@
 from models import TournamentModel
 from datetime import datetime
 import os
+import random
 
 
 class TournamentController:
@@ -161,3 +162,48 @@ class TournamentController:
                 user_input = self.view.message_select_player2_tournamentview() # Demande à l'utilisateur de saisir le joueur à ajouter au tournoi
                 self.db.add_player_in_tournament_db(user_input)
                 self.input_control_player_in_tournament()
+
+    def start_tournament(self):
+        if self.controller.player_list_controller.control_player_select_controller():
+            if not self.controller.player_list_controller.control_number_player_in_list_players_select():
+                self.shuffle_players()
+            else:
+                self.view.message_message_start_tournament_missing_player()
+        else:
+            self.view.message_message_start_tournament_missing_player()
+
+    def shuffle_players(self):
+        """Mélanger les joueurs inscrits"""
+        temporary_list = []
+        player_number = 0
+        color_1 = "Blanc"
+        color_2 = "Noir"
+        list_player_select = self.db.get_list_player_select_db()
+        random.shuffle(list_player_select)
+        """Assigner une couleur"""
+        for list_player_select1 in list_player_select:
+            color_1, color_2 = color_2, color_1
+            player_number += 1
+            player = {
+                "couleur": color_1,
+                "identifiant": list_player_select1["identifiant"],
+                "nom": list_player_select1["nom"],
+                "prenom": list_player_select1["prenom"],
+                "naissance": list_player_select1["naissance"],
+                "joueur": player_number,
+                "round": list_player_select1["round"],
+                "score": list_player_select1["score"]
+            }
+            temporary_list.append(player)
+
+        #self.create_teams(temporary_list)
+        self.db.add_player_shuffle_in_list_match_json(temporary_list)
+        self.create_teams()
+
+
+    def create_teams(self):
+        temporary_list = self.db.get_player_list_match()
+        self.view.print_match(temporary_list)
+
+
+
