1de8225f5a1cbcb3d23e25b7a8c0a48ef5a277f8
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-21 23:43:06 +0200

    Ajout de la méthode round_file_control qui permet de controller la présence du fichier RoundX.json. Ajout de la méthode control_number_player_in_list_round qui permet de connaitre le nombre de joueurs qu'il y a dans le fichier RoundX.json. Ajour de la méthode list_match_file_control qui pemet de controller l'existance du fichier List_MatchX.json

--- controllers/playerlistcontroller.py
+++ controllers/playerlistcontroller.py
@@ -22,6 +22,7 @@ class PlayerListController:
         list_player_select = self.db.get_list_player_select_db()
         self.view.print_list_player_select()
         self.view.print_player_list_view(list_player_select)
+        self.controller.menu_controller.run_tournament_menu()
 
 
     def del_player_in_list_controller(self):
@@ -46,7 +47,7 @@ class PlayerListController:
         if self.control_player_select_controller():
             list_player_select = self.db.get_list_player_select_db() # Récupération de la liste des joueurs inscrits
             player_sorted = self.db.sort_player_list_db(list_player_select)  # Tries la liste des joueurs par ordre alphabétique
-            self.view.print_list_player_select() # Affichage "Liste des joueurs inscrits au tournoi"
+            self.view.print_list_player_select() # Affiche "Liste des joueurs inscrits au tournoi"
             self.view.print_player_list_view(player_sorted) # Affiche la liste des joeuurs inscrits dans l'ordre alphabétique
             user_input = self.view.message_del_player_in_list_view() # Demande à l'utilisateur de faire son choix
             if user_input == "":
@@ -84,7 +85,7 @@ class PlayerListController:
         path = f"{data}/data/tournament/"
         directory = os.listdir(path)
         tournament_name = str(directory[0])
-        path2 = (f"{data}/data/tournament/{tournament_name}/Player_Select")
+        path2 = f"{data}/data/tournament/{tournament_name}/Player_Select"
         directory_player_select = os.listdir(path2)
         if directory_player_select:
             return True
@@ -99,8 +100,44 @@ class PlayerListController:
             temporary_list.append(list_player_select1)
         numbers_players = len(temporary_list)
         if numbers_players == MAX_PLAYER:
-            return False
-
-        else:
             return True
 
+        else:
+            return False
+
+
+    def round_file_control(self):
+        """Méthode qui controle la présence du fichier Round"""
+        data = os.getcwd()
+        path = f"{data}/data/tournament/"
+        directory = os.listdir(path)
+        tournament_name = str(directory[0])
+        path2 = f"{data}/data/tournament/{tournament_name}/Rounds"
+        directory_rounds = os.listdir(path2)
+        if directory_rounds:
+            return True
+        else:
+            return False
+
+    def control_number_player_in_list_round(self):
+        """Méthode qui controle combien de joueur se trouvent dans le fichier RoundX.json"""
+        temporary_list = []
+        list_round = self.db.get_list_round()
+        for list_round1 in list_round:
+            temporary_list.append(list_round1)
+        numbers_players = len(temporary_list)
+        numbers_players = int(numbers_players)
+        return numbers_players
+
+    def list_match_file_control(self):
+        """Méthode qui permet de controler l'existance du fichier List_MatchX.json"""
+        data = os.getcwd()
+        path = f"{data}/data/tournament/"
+        directory = os.listdir(path)
+        tournament_name = str(directory[0])
+        path2 = f"{data}/data/tournament/{tournament_name}/Match"
+        file_rounds = os.listdir(path2)
+        if file_rounds:
+            return True
+        else:
+            return False
