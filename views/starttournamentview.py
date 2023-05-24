d0089fd6423556fe47c9d7a9907e96321e29baf8
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-24 16:24:03 +0200

    Ajout de la méthode tournament_nnot_lauched qui permet d'afficher un message à l'utilisateur

--- views/starttournamentview.py
+++ views/starttournamentview.py
@@ -6,6 +6,9 @@ class StartTournamentView:
     def message_tournament_launched(self):
         print("Le tournoi a déja été lancé, vous pouvez saisir les scores des joueurs.")
 
+    def tournament_not_launched(self):
+        print("Vous devez lancer le tournoi avant de saisir les scores")
+
     def message_start_tournament_missing_player(self):
         print("Action impossible, vous devez ajouter des joueurs dans le tournoi")
 
@@ -19,7 +22,7 @@ class StartTournamentView:
         print("============ Saisie des scores ===========")
 
     def winner_player_selection(self):
-        user_input = input("Saisir le numéro du joueur qui remporte la partie ( 1 ou 2)")
+        user_input = input("Saisir le numéro du joueur qui remporte le match ( 1 ou 2):")
         return user_input
     def get_user_input_match_for_score_entry(self):
         user_input = input("Saisir le numéro du joueur pour la saisie du score (1, 2, 3 ...) ")
@@ -66,32 +69,3 @@ class StartTournamentView:
         for list_match1 in list_match:
             i += 1
             print(f"-{i}-", "Nom :", list_match1["nom"], list_match1["prenom"])
-
-
-
-    def print_team_1(self, list_match):
-        select1 = list_match[0]
-        select2 = list_match[1]
-        print(f"Équipe 1 :")
-        print("-1-", select1["nom"], select1["prenom"])
-        print("-2-", select2["nom"], select2["prenom"])
-    def print_team_2(self, list_match):
-        select3 = list_match[2]
-        select4 = list_match[3]
-        print(f"Équipe 2 :")
-        print("-1-", select3["nom"], select3["prenom"])
-        print("-2-", select4["nom"], select4["prenom"])
-
-    def print_team_3(self, list_match):
-        select5 = list_match[4]
-        select6 = list_match[5]
-        print(f"Équipe 3 :")
-        print("-1-", select5["nom"], select5["prenom"])
-        print("-2-", select6["nom"], select6["prenom"])
-    def print_team_4(self, list_match):
-        select7 = list_match[6]
-        select8 = list_match[7]
-        print(f"Équipe 4 :")
-        print("-1-", select7["nom"], select7["prenom"])
-        print("-2-", select8["nom"], select8["prenom"])
-
