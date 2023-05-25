eabdfc76a49e44d8f918dcb5e73579521f927dd3
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-25 16:29:15 +0200

    Ajout de la méthode next_match qui permet d'enregistrer les données des joueurs suite à la création d'un match suivant. Ajout de plusieurs appels de méthode qui permet de créer le fichier Final_Scores.json lorsque le tournoi est terminé

--- controllers/starttournamentcontroller.py
+++ controllers/starttournamentcontroller.py
@@ -2,11 +2,12 @@ import random
 import time
 
 class StartTournamentController:
-    def __init__(self, view, controller, database, directory):
+    def __init__(self, view, controller, database, directory, match):
         self.view = view
         self.controller = controller
         self.db = database
         self.dm = directory
+        self.mc = match
 
     def start_tournament(self):
         if self.controller.player_list_controller.list_match_file_control():
@@ -49,6 +50,34 @@ class StartTournamentController:
         self.db.add_player_shuffle_in_list_match_json(temporary_list)
         self.create_teams()
 
+    def next_match(self, new_list_match):
+        score = 0.0
+        number_files = self.controller.player_list_controller.number_files_round()
+        number_files += 1
+        dateiso = time.strftime('%d/%m/%Y - %H:%M')
+        temporary_list = []
+        color_1 = "Blanc"
+        color_2 = "Noir"
+
+        """Ajouter la date et l'heure de début et assigner une couleur"""
+        for new_list_match1 in new_list_match:
+            color_1, color_2 = color_2, color_1
+            player = {
+                "Date et heure de debut": dateiso,
+                "couleur": color_1,
+                "identifiant": new_list_match1["identifiant"],
+                "nom": new_list_match1["nom"],
+                "prenom": new_list_match1["prenom"],
+                "naissance": new_list_match1["naissance"],
+                "joueur": new_list_match1["joueur"],
+                "round": number_files,
+                "score": score
+            }
+            temporary_list.append(player)
+
+        self.db.add_player_shuffle_in_list_match_json(temporary_list)
+        self.create_teams()
+
     def create_teams(self):
         temporary_list = self.db.get_player_list_match()
         self.view.message_start_tournament()
@@ -61,7 +90,10 @@ class StartTournamentController:
             self.view.tournament_not_launched()
             self.controller.menu_controller.run_tournament_menu()
         """Si le fichier Round1.json n'existe pas"""
-        if not self.controller.player_list_controller.round_file_control():
+        number_files_round = self.controller.player_list_controller.number_files_round()
+        number_files_match = self.controller.player_list_controller.number_files_in_list_match()
+        if number_files_match > number_files_round:
+        #if not self.controller.player_list_controller.round_file_control():
             list_match = self.db.get_player_list_match() # Récupération des données de List_MatchX.json
             self.view.view_match_for_score_entry(list_match) # Affiche à l'utilisateur la liste des joueurs
             choice_player1 = self.view.get_user_input_match_for_score_entry() # Demande à l'utilisateur de choisir un joueur pour entrer le score
@@ -71,6 +103,7 @@ class StartTournamentController:
             self.team_selection(choice_player1)
 
         """Si le fichier Round1.json existe"""
+
         list_match = self.db.get_list_match_and_round() # Récupération des joueurs n'ayant pas eu de score
         self.view.view_match_for_score_entry(list_match)  # Affiche à l'utilisateur la liste des joueurs
         """Control de la saisie utilisateur"""
@@ -82,7 +115,10 @@ class StartTournamentController:
 
     def team_selection(self, choice_player1):
         """Si le fichier Round1.json n'existe pas"""
-        if not self.controller.player_list_controller.round_file_control():
+        number_files_round = self.controller.player_list_controller.number_files_round()
+        number_files_match = self.controller.player_list_controller.number_files_in_list_match()
+        if number_files_match > number_files_round:
+        #if not self.controller.player_list_controller.round_file_control():
             choice_score1 = self.view.get_user_input_match_for_score_entry2() # Demande Match Nul ou Gagnant / Perdant
             self.user_input_control2(choice_score1)
             choice_score1 = int(choice_score1)
@@ -118,14 +154,41 @@ class StartTournamentController:
                 self.view.end_of_game_message()
                 """Crer le fichier des scores"""
                 self.create_file_score_controller()
-                """Récupération des données de ScoreX.json pour créer le match suivant"""
-                list_score = self.db.get_list_scores()
-                list_score_sorted = self.db.sort_player_list_score(list_score)
-                self.db.add_player_shuffle_in_list_match_json(list_score_sorted)
-                """Afficher Le match suivant"""
+                number_files_score = self.controller.player_list_controller.number_of_score_files()
+                if number_files_score == 4:
+                    """Créer un fichier final_score qui reprend tous les fichiers scores afin de comptabilisé 
+                        les scores des joueurs 
+                    """
+                    self.db.add_scores_to_final_score_file()
+                    """Afficher un message indiquant la fin du tournoi"""
+                    self.view.message_end_tournament()
+                    """Récupérer les infos du dernier fichier score"""
+                    """Afficher le joueur qui remporte le tournoi, """
+                    """Si égalité afficher les joueurs ex-aequo"""
+                    """Déplacer le fichier portant le nom du tournoi dans tournament_old """
+                    """Ajouter à la suite du nom du tournoi la date du jour"""
+                    """Revenir au menu principal"""
+                if number_files_score <= 3:
+                    temporary_list_match = self.db.get_player_list_match()  # Obtenir List_Match
+                    temporary_list_round = self.db.get_list_round()  # Obtenir List_Round
 
-                """Retour au menu GESTIONNAIRE DU TOURNOI"""
-                self.controller.menu_controller.run_tournament_menu()
+                    list_round_sorted = self.db.sort_player_list_score(temporary_list_round)  # Trier par score
+
+                    match_list = self.mc.make_team_list_match(temporary_list_match)  # Créer équipe list_Match
+
+                    round_list = self.mc.make_team_list_round(list_round_sorted)  # Créer équipe list_Round
+
+                    numbers_player_match = self.mc.extract_player_number_from_list_match(match_list)
+                    number_player_round = self.mc.extract_player_number_from_list_round(round_list)
+
+                    final_list = self.mc.sorted_match_list_and_round(numbers_player_match, number_player_round)
+                    new_list_match = self.mc.player_selection_from_list_match(final_list)
+                    """Afficher Le match suivant"""
+                    self.next_match(new_list_match)
+
+                    self.view.print_start_match(new_list_match)
+                    """Retour au menu GESTIONNAIRE DU TOURNOI"""
+                    self.controller.menu_controller.run_tournament_menu()
 
             else:
                 self.input_control_player_selection()
@@ -148,14 +211,43 @@ class StartTournamentController:
                 self.view.end_of_game_message()
                 """Créer le fichier des scores"""
                 self.create_file_score_controller()
-                """Récupération des données de ScoreX.json pour créer le match suivant"""
-                list_score = self.db.get_list_scores()
-                list_score_sorted = self.db.sort_player_list_score(list_score)
-                self.db.add_player_shuffle_in_list_match_json(list_score_sorted)
-                """Afficher Le match suivant"""
+                number_files_score = self.controller.player_list_controller.number_of_score_files()
+                if number_files_score == 4:
+                    """Créer un fichier final_score qui reprend tous les fichiers scores afin de comptabilisé 
+                        les scores des joueurs 
+                    """
+                    self.db.add_scores_to_final_score_file()
+                    """Afficher un message indiquant la fin du tournoi"""
+                    self.view.message_end_tournament()
+                    """Récupérer les infos du dernier fichier score"""
+                    """Afficher le joueur qui remporte le tournoi, """
+                    """Si égalité afficher les joueurs ex-aequo"""
+                    """Déplacer le fichier portant le nom du tournoi dans tournament_old """
+                    """Ajouter à la suite du nom du tournoi la date du jour"""
+                    """Revenir au menu principal"""
+                    self.controller.menu_controller.run_tournament_menu()
+                if number_files_score <= 3:
+                    """Créer le match suivant"""
+                    temporary_list_match = self.db.get_player_list_match()  # Obtenir List_Match
+                    temporary_list_round = self.db.get_list_round()  # Obtenir List_Round
 
-                """Retour au menu GESTIONNAIRE DU TOURNOI"""
-                self.controller.menu_controller.run_tournament_menu()
+                    list_round_sorted = self.db.sort_player_list_score(temporary_list_round)  # Trier par score
+
+                    match_list = self.mc.make_team_list_match(temporary_list_match)  # Créer équipe list_Match
+
+                    round_list = self.mc.make_team_list_round(list_round_sorted)  # Créer équipe list_Round
+
+                    numbers_player_match = self.mc.extract_player_number_from_list_match(match_list)
+                    number_player_round = self.mc.extract_player_number_from_list_round(round_list)
+
+                    final_list = self.mc.sorted_match_list_and_round(numbers_player_match, number_player_round)
+                    new_list_match = self.mc.player_selection_from_list_match(final_list)
+                    """Afficher Le match suivant"""
+                    self.next_match(new_list_match)
+
+                    self.view.print_start_match(new_list_match)
+                    """Retour au menu GESTIONNAIRE DU TOURNOI"""
+                    self.controller.menu_controller.run_tournament_menu()
             else:
                 self.input_control_player_selection()
 
