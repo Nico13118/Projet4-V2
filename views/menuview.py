0cde4fd781f2039b905b0ce01cc6f6894b0fc155
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-27 17:25:11 +0200

    Ajout de la méthode repeat_message qui permet d'afficher un message et de récupérer la saisie de l'utilisateur. Suppression des input au niveau des menus

--- views/menuview.py
+++ views/menuview.py
@@ -7,7 +7,7 @@ class MenuView:
         """
         Affichage du menu
         Récupération de la saisie utilisateur
-        :return: Retourne la saisie utilisateur
+        : return : Retourne la saisie utilisateur
         """
         print("\n|============= MENU PRINCIPAL ==============|")
         print("| 1 -Gestionnaire de joueurs                |")
@@ -15,9 +15,6 @@ class MenuView:
         print("| 3 -Rapport de tournoi                     |")
         print("| 4 -Quitter l'application.                 |")
         print("|===========================================|")
-
-        user_input = input("Faite votre choix :")
-        return user_input
 
     def get_user_input_player_menu(self):
 
@@ -27,9 +24,6 @@ class MenuView:
         print("| 3 -Supprimer un joueur de la liste        |")
         print("| 4 -Retourner au menu principal            |")
         print("|===========================================|")
-
-        user_input = input("Faite votre choix :")
-        return user_input
 
     def get_user_input_tournament_menu(self):
 
@@ -45,24 +39,19 @@ class MenuView:
         print("| 9 -Retourner au menu principal            |")
         print("|===========================================|")
 
-        user_input = input("Faite votre choix :")
-        return user_input
-
     def get_user_input4(self):
-
-        def print_menu_report():
-            print("\n|=========== RAPPORT DE TOURNOI ===========|")
-            print("| 1 -Rapport du tournoi en cours            |")
-            print("| 2 -Rapport des tournois précédents.       |")
-            print("| 3 -Retourner au menu principal            |")
-            print("|===========================================|")
-
-        print_menu_report()
-        user_input = input("Faite votre choix :")
-        return user_input
+        print("\n|=========== RAPPORT DE TOURNOI ===========|")
+        print("| 1 -Rapport du tournoi en cours            |")
+        print("| 2 -Rapport des tournois précédents.       |")
+        print("| 3 -Retourner au menu principal            |")
+        print("|===========================================|")
 
     def error_message_menuview1(self):
         print("Choix invalide !!!")
 
     def error_message_menuview2(self):
         print("La saisie ne peut pas être vide !!")
+
+    def repeat_message(self):
+        user_input = input("Faite votre choix :")
+        return user_input
