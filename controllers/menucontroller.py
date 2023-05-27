c5e6c2100446f474fb5185bd43bdbd8e38e2ad20
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-27 16:50:59 +0200

    Ajout de la méthode control_user_input qui permet de controller la saisie de l'utilisateur lors de son choix dans les menus. Suite à l'ajout de cette méthode des modifications ont été apportées dans les autres méthodes des menus

--- controllers/menucontroller.py
+++ controllers/menucontroller.py
@@ -9,106 +9,112 @@ class MenuController:
     def run_menu(self):
         """"MENU PRINCIPAL"""
         self.view.club_name()
-        user_input = self.view.get_user_input_main_menu()
-        if user_input == "1":
+        number = 4
+        self.view.get_user_input_main_menu()
+        user_input = self.control_user_input(number)
+        if user_input == 1:
             """Gestionnaire des joueurs"""
             self.run_menu_player()
-        elif user_input == "2":
+        elif user_input == 2:
             """Gestionnaire des tournois"""
             self.run_tournament_menu()
-        elif user_input == "3":
+        elif user_input == 3:
             """Rapport de tournoi"""
             self.run_menu_report()
-        elif user_input == "4":
+        elif user_input == 4:
             """Quitter l'application"""
             quit()
-        elif user_input > "4":
-            self.view.error_message_menuview1()
-            self.run_menu()
-        else:
-            self.view.error_message_menuview2()
-            self.run_menu()
+
 
     def run_menu_player(self):
         """Gestionnaire de joueurs"""
         self.view.club_name()
-        user_input = self.view.get_user_input_player_menu()
-        if user_input == "1":
+        number = 4
+        self.view.get_user_input_player_menu()
+        user_input = self.control_user_input(number)
+        if user_input == 1:
             """Inscription des joueurs"""
             self.controller.player_controller.player_message()
             self.run_menu_player()
-        elif user_input == "2":
+        elif user_input == 2:
             """Voir la liste des joueurs"""
             self.controller.player_list_controller.print_player_list_controller()
-        elif user_input == "3":
+        elif user_input == 3:
             """Supprimer un joueur de la liste"""
             self.controller.player_list_controller.del_player_in_list_controller()
-            self.run_menu_player()
-        elif user_input == "4":
+        elif user_input == 4:
             """Retourner au menu principal"""
             self.run_menu()
-        elif user_input > "4":
-            self.view.error_message_menuview1()
-            self.run_menu_player()
-        else:
-            self.view.error_message_menuview2()
-            self.run_menu_player()
 
     def run_tournament_menu(self):
         """Gestionnaire de tournoi"""
         self.view.club_name()
-        user_input = self.view.get_user_input_tournament_menu()
-        if user_input == "1":
+        number = 9
+        self.view.get_user_input_tournament_menu()
+        user_input = self.control_user_input(number)
+        if user_input == 1:
             """Créer un tournoi"""
-            self.controller.tournament_controller.control_tournament_directory()
-        elif user_input == "2":
+            self.controller.tournament_controller.tournament_registration()
+        elif user_input == 2:
             """Ajouter des joueurs dans le tournoi"""
             self.controller.tournament_controller.add_player_in_tournament_controller()
             self.run_tournament_menu()
-        elif user_input == "3":
+        elif user_input == 3:
             """liste des joueurs inscrits au tournoi"""
             self.controller.player_list_controller.print_list_player_select()
-        elif user_input == "4":
+        elif user_input == 4:
             """Supprimer des joueurs du tournoi"""
             self.controller.player_list_controller.del_player_in_list_player_select()
-        elif user_input == "5":
+        elif user_input == 5:
             """Lancer le tournoi"""
             self.controller.start_tournament_controller.start_tournament()
-        elif user_input == "6":
+        elif user_input == 6:
             """Saisir les scores"""
             self.controller.start_tournament_controller.player_selection()
-        elif user_input == "7":
+        elif user_input == 7:
             """Tableau des équipes"""
             self.controller.start_tournament_controller.show_team_table_controller()
-        elif user_input == "8":
+        elif user_input == 8:
             """Supprimer un tournoi"""
             self.controller.tournament_controller.del_tournament_directory()
-        elif user_input == "9":
+        elif user_input == 9:
             """Retourner au menu principal"""
             self.run_menu()
-        elif user_input > "7":
-            self.view.error_message_menuview1()
-            self.run_tournament_menu()
-        else:
-            self.view.error_message_menuview2()
-            self.run_tournament_menu()
 
     def run_menu_report(self):
         """Rapport de tournoi"""
         self.view.club_name()
-        user_input = self.view.get_user_input4()
-        if user_input == "1":
+        number = 3
+        self.view.get_user_input4()
+        user_input = self.control_user_input(number)
+        if user_input == 1:
             """Rapport du tournoi en cours"""
             pass
-        elif user_input == "2":
+        elif user_input == 2:
             """Rapport des tournois précédents"""
             pass
-        elif user_input == "3":
+        elif user_input == 3:
             """Retourner au menu principal"""
             self.run_menu()
-        elif user_input > "3":
-            self.view.error_message_menuview1()
-            self.run_menu_report()
-        else:
-            self.view.error_message_menuview2()
-            self.run_menu_report()
+
+    def control_user_input(self, number):
+        user_input2 = True
+        while user_input2:
+            user_input = self.view.repeat_message()
+            if user_input == "":
+                self.view.error_message_menuview2()
+            if str(user_input).isalpha():
+                self.view.error_message_menuview1()
+            if user_input != "" and not str(user_input).isalpha():
+                user_input = int(user_input)
+                if user_input == 0:
+                    self.view.error_message_menuview1()
+                if user_input > number:
+                    self.view.error_message_menuview1()
+                if user_input <= number:
+                    return user_input
+
+
+
+
+
