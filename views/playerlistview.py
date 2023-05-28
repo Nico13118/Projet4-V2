12fa1a96ce7bd92beda11930660cb07959dddce2
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-05-28 14:24:47 +0200

    Ajout de plusieurs méthodes afin d'afficher des messages d'erreurs à l'utilisateur

--- views/playerlistview.py
+++ views/playerlistview.py
@@ -13,14 +13,14 @@ class PlayerListView:
         print("Saisir le numéro du joueur à supprimer de la liste (1, 2, 3 ...) :")
 
     def repeat_message(self):
-        user_input = input("Quel est votre choix ?:")
+        user_input = input("Faite votre choix :")
         return user_input
 
     def message_del_player_in_list_view2(self):
         print("Joueur supprimé avec succes.")
 
     def message_error_list_view(self):
-        print("Aucune liste à afficher")
+        print("Action impossible !! Aucun joueurs n'a été ajouté au tournoi.")
 
     def message_error_list_view2(self):
         print("Choix invalide !!!")
@@ -30,6 +30,15 @@ class PlayerListView:
 
     def message_error_list_view4(self):
         print("Saisie incorrect, veuillez réessayer !")
+
+    def message_no_list(self):
+        print("Action impossible !! Vous devez procéder à l'enregistrement des joueurs.")
+
+    def message_no_tournament_directory(self):
+        print("Action impossible !! Aucun tournoi n'a été crée.")
+
+    def tournament_already_launched(self):
+        print("Impossible de supprimer un joueur de la liste car le tournoi a commencé.")
 
     def print_player_list_view(self, list_player):
         i = 0
