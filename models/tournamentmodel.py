541892499dfe0928174d8a333d3bdd97902f6f3e
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-06-02 11:43:13 +0200

    Dans la méthode tournament_registration, ajout de la ligne rounds

--- models/tournamentmodel.py
+++ models/tournamentmodel.py
@@ -1,10 +1,11 @@
 class TournamentModel:
-    def __init__(self, tournament_name, place, start_date, end_date, director_remark):
+    def __init__(self, tournament_name, place, start_date, end_date, director_remark, number_rounds):
         self.tournament_name = tournament_name
         self.place = place
         self.start_date = start_date
         self.end_date = end_date
         self.director_remark = director_remark
+        self.number_rounds = number_rounds
         self.tournament = []
 
     def tournament_registration(self):
@@ -13,7 +14,8 @@ class TournamentModel:
             "lieu": self.place,
             "date1": self.start_date,
             "date2": self.end_date,
-            "remarques": self.director_remark
+            "remarques": self.director_remark,
+            "rounds": self.number_rounds
         }
         self.tournament.append(tournament_information)
         tournament = self.tournament
