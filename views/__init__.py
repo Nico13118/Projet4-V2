e8299e59d2ccdca08bb08ac0c07e3612ea1f6f5e
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-06-05 18:36:00 +0200

    Déplacement de toutes les importations car pose problème avec flak8

--- views/__init__.py
+++
@@ -1,6 +0,0 @@
-from .menuview import MenuView
-from .tournamentview import TournamentView
-from .playerview import PlayerView
-from .playerlistview import PlayerListView
-from .starttournamentview import StartTournamentView
-from .reportview import ReportView
